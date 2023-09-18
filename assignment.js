db44d75f254b7e963ccbeaa5a2b7789172579c29
Asaduzzman <145313356+asaduzzaman2031@users.noreply.github.com>
2023-09-18 19:15:59 +0600

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,56 @@
+// ১. Variable কি ?
+// Ans:একটি নির্দিষ্ট প্রতীক যেটি একটি নির্দিষ্ট মান অথবা Value উপস্থাপন করে এবং সংরক্ষণ করে |
+
+// ২. Variable কিভাবে লিখতে হয় ?
+// Ans:  var যা Variable  এর   সংক্ষিপ্ত রূপ | তাই প্রথমে var তারপর Meanign full word এবং সমান চিহ্ন| double cotesion ar modhe Input |
+// Ex: var ourCountry= "Bangladesh"; console.log(ourCountry) ;
+
+// ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+// Ans: কোন শব্দ বা ওয়ার্ড সংরক্ষণের জন্য string type variable ব্যবহার করা হয় |
+// Ex: var strType="Hello World"; console.log(strType) ;
+
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+// Ans: গাণিতিক প্রতীক বা চিহ্ন সংখ্যা সংরক্ষণের জন্য numbertype variable ব্যবহার করা হয় |
+// Ex: var numType= 50; console.log(numType) ;
+
+// // ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+// Ans: কোন শর্ত অথবা true অথবা false লজিকের জন্য Boolean type variable  ব্যবহার করা হয় |
+// Ex: var boolType =true; or var boolType = false; console.log(boolType) ;
+
+// ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+// Ans: Capital Letter কে Small Letter এ রূপান্তরের জন্য toLowerCase() এবং  Small Letter কে Capital  Letter এ রূপান্তরের জন্য toUpperrCase() ব্যবহার করা হয় ;
+// Ex: var ourCountry ="BANGLADESH.BAGLADESH IS AN INDEPENDENT COUNTRY";
+// console.log(ourCountry.toLowerCase()); & console.log(ourCountry.toUpperCase());
+
+// ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+// Ans: Arithmatic javascript এ অপারেটর রয়েছে পাঁচটি তা হল : যোগ বিয়োগ গুণ ভাগ এবং ভাগশেষ (+, - , * , / %) ;
+
+// ৮. Math.abs() এর ব্যাবহার লিখুন । 
+// Ans: var num1=-500;
+// var sum= Math.abs(num1);
+// console.log(sum)
+
+// ৯. Math.celi()  এর ব্যাবহার লিখুন । 
+// Ans: var num=0.000002;
+// var sum=Math.ceil(num);
+// console.log(sum);
+
+// ১০. Math.Floor() এর ব্যাবহার লিখুন । 
+// Ans: var num=0.999999;
+// var sum=Math.floor(num);
+// console.log(sum);
+
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+// Ans: var num= 0.6;
+// var sum=Math.round(num);
+// console.log(sum);
+// OR 
+// var num2=0.3;
+// var sum=Math.round(num2)
+// console.log(sum);
+
+// // ১২. Math.random() এর ব্যাবহার লিখুন ।
+// Ans: var lottery= Math.random();
+// console.log(lottery);
+
+// Asaduzzamn
